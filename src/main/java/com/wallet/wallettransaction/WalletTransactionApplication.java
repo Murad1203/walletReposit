6f0de2e2aa465e24d77bbebfe0003d6d1ff864d1
Murad1203 <lutkun666@gmail.com>
2024-11-25 21:02:01 +0300

    first commit

---
+++ src/main/java/com/wallet/wallettransaction/WalletTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.wallet.wallettransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletTransactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletTransactionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wallet/wallettransaction/WalletTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallet.wallettransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletTransactionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
